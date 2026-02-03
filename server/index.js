04522f45ede629dc23510e4b9a4f699172c0f412
Jesus Pestana <jesuspestana77@gmail.com>
2026-02-03 09:45:12 -0400

    Added .gitignore file and fixed variables on index.js. Added new .env variable

--- server/index.js
+++ server/index.js
@@ -4,12 +4,12 @@ import dotenv from 'dotenv'
 dotenv.config();
 
 // Debug info
-console.log('Connecting to database:', process.env.CONNECTION_MARIADB)
+console.log('Connecting to database:', process.env.CONNECTION_STRING)
 
 // Initialize instance of Sequelize
 const sequelize = new Sequelize(process.env.CONNECTION_MARIADB, {
     logging: console.log(),
-    dialect: 'mariadb',
+    dialect: process.env.DB,
     dialectOptions: {
         ssl: false
     }
@@ -20,8 +20,10 @@ sequelize.authenticate()
     .then(async () => {
         console.log('Database connection is: OK');
         // check database information
-        // const [results] = await sequelize.query('SELECT current_database(), current_schema()');
-        // console.log('Connected to:', results[0])
+        if (process.env.DB === 'postgresql') {
+            const [results] = await sequelize.query('SELECT current_database(), current_schema()');
+            console.log('Connected to:', results[0])
+        }
     }) .catch(error => {
         console.error('Unable to connecto to current database:', error)
     })
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,62 +1,76 @@
 import { Router } from "express";
 import { User } from "../models/userModel.js";
 import { sequelize } from "../index.js";
-import bcrypt from "bcrypt"
 
 const router = Router();
 
-router.get('/', async (request, response) => {
-    try {
-        const users = await User.findAll({
-            attributes: ["id", "fullName", "created_at"]
-        });
-        return response.json({ users });
-    } catch (error) {
-        console.error("Something went wrong:", error);
-        return response.status(500).json({ message: "Internal server error" });
-    }
+// GET all users
+router.get("/", async (request, response) => {
+  try {
+    const users = await User.findAll();
+    return response.json({ users });
+  } catch (error) {
+    console.error("Something went wrong:", error);
+    return response.status(500).json({ message: "Internal server error" });
+  }
 });
 
-router.post('/', async (request, response) => {
-    const transaction = await sequelize.transaction();
-    try {
-        let data = request.body;
-        if(!data.email || !data.password || !data.fullName) {
-            return response.status(400).json({ message: "Missing required fields"})
-        }
+// Create a user
+router.post("/", async (request, response) => {
+  const t = await sequelize.transaction();
+  try {
+    let data = request.body;
 
-        const userExist = await User.findOne({
-            where: {
-                email: data.email
-            },
-            transaction
-        })
+    const userExist = await User.findOne({
+      where: {
+        email: data.email,
+      }});
 
-        if (userExist) {
-            await transaction.rollback();
-            return response.status(409).json({ message: "User already exists"})
-        }
-
-        //Hashing of the password
-        const newUser = await User.create({
-            fullName: data.fullName,
-            email: data.email,
-            password: data.password
-        }, { transaction })
-
-        await transaction.commit();
-        return response.status(201).json({ success: true, data: {
-            userId: newUser.id, 
-            email: newUser.email
-        }});
-
-    } catch (error) {
-        await transaction.rollback();
-        console.error("Error creating user:", error);
-        return response.status(500).json({
-            message: "An error has ocurred during user creation:", error
-        })
+    if (userExist) {
+      return response.status(409).json({ message: "User already exists" });
     }
 
-})
+    // create new user
+    const newUser = await User.create(
+      {
+        fullName: data.fullName,
+        email: data.email,
+        password: data.password,
+      },
+        { t },
+    );
+
+    await t.commit();
+    return response.status(201).json({
+      success: true,
+      data: {
+        userId: newUser.id,
+        email: newUser.email,
+      },
+    });
+  } catch (error) {
+    await t.rollback()
+    // Check if body data is ok
+    if (
+      error.name === "SequelizeUniqueConstraintError" ||
+      error.name === "SequelizeValidationError"
+    ) {
+      return response
+        .status(400)
+        .json({
+          status: "error",
+          message: "Body data is incomplete or unique",
+          error: error.errors.map(err => ({
+            field: err.path,
+            message: err.message
+          }))
+        });
+    }
+    console.error("Error creating user:", error);
+    return response.status(500).json({
+      message: "An error has ocurred during user creation:",
+      error,
+    });
+  }
+});
 export default router;
